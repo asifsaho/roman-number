ccc38add7ad1d337f023cef1fcc4f5ffc54025b4
asifsaho <md-asif.nawaz@akelius.de>
2020-09-27 00:53:45 +0200

    UI activation with converter function

--- src/NumberInput.jsx
+++ src/NumberInput.jsx
@@ -1,27 +1,36 @@
 import React, { useState } from "react";
+import numberToRomanConverter from "./numberConverter";
 
 const NumberInput = () => {
   const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
   const numberChange = (event) => {
     setInput(event.target.value);
   };
 
   const convertNumber = (event) => {
     event.preventDefault();
-    setInput("");
-    console.log(input);
+    const output = numberToRomanConverter(Number(input));
+    setOutput(output);
   };
 
   return (
-    <form onSubmit={convertNumber}>
-      <input
-        className="input"
-        onChange={numberChange}
-        value={input}
-        placeholder="write roman or decimal number"
-      />
-      <button type="submit">convert</button>
-    </form>
+    <div className="wrapper">
+      <form onSubmit={convertNumber}>
+        <input
+          className="input"
+          onChange={numberChange}
+          value={input}
+          placeholder="write roman or decimal number"
+        />
+        <button type="submit">convert</button>
+      </form>
+      {!!output.length && (
+        <div>
+          your expected output is <strong>{output}</strong>
+        </div>
+      )}
+    </div>
   );
 };
 
