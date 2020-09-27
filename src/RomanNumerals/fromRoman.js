1653704a7342ebebb1ce8f16dd9f50eb9721db13
asifsaho <md-asif.nawaz@akelius.de>
2020-09-27 10:07:27 +0200

    from roman function

--- src/RomanNumerals/fromRoman.js
+++ src/RomanNumerals/fromRoman.js
@@ -1,24 +1,39 @@
 const romanDictionary = {
-  1: "I",
-  4: "IV",
-  5: "V",
-  9: "IX",
-  10: "X",
-  40: "XL",
-  50: "L",
-  90: "XC",
-  100: "C",
-  400: "CD",
-  500: "D",
-  900: "CM",
-  1000: "M",
+  I: 1,
+  IV: 4,
+  V: 5,
+  IX: 9,
+  X: 10,
+  XL: 40,
+  L: 50,
+  XC: 90,
+  C: 100,
+  CD: 400,
+  D: 500,
+  CM: 900,
+  M: 1000,
 };
 
-const romanToNumberConverter = (num) => {
-  let output = 0;
-  // MCMLXXXIX
+const exceptionNumbers = ["IV", "IX", "XL", "XC", "CD", "CM"];
 
-  return num;
+const romanNumbers = Object.keys(romanDictionary);
+
+const romanToNumberConverter = (roman) => {
+  let output = 0;
+
+  exceptionNumbers.forEach((key) => {
+    if (roman.includes(key)) {
+      output += romanDictionary[key];
+      roman = roman.split(key).join("");
+    }
+  });
+
+  roman.split("").forEach((key) => {
+    output += romanDictionary[key];
+    roman = roman.split(key).join("");
+  });
+
+  return output;
 };
 
 export default romanToNumberConverter;
